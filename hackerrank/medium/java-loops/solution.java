60e7a54ad9e2219bf6396f5d3cae7b1c31f87316
chakrabortyayush1805-maker <chakrabortyayush1805@gmail.com>
2026-08-03 16:45:16 +0530

    ✅ Java Loops II | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-03

--- hackerrank/medium/java-loops/solution.java
+++ hackerrank/medium/java-loops/solution.java
@@ -1,16 +1,25 @@
-import java.io.*;
+import java.util.Scanner;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-
-        int N = Integer.parseInt(bufferedReader.readLine().trim());
-
-        for (int i = 1; i <= 10; i++) {
-            int result = N * i;
-            System.out.println(N + " x " + i + " = " + result);
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        
+        for (int q = 0; q < t; q++) {
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+            int currentSum = a;
+            
+            for (int i = 0; i < n; i++) {
+                currentSum += (1 << i) * b;
+                System.out.print(currentSum + " ");
+            }
+            
+            System.out.println();
         }
-
-        bufferedReader.close();
+        
+        in.close();
     }
 }
