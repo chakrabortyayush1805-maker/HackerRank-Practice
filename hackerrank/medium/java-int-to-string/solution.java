dfc0186bf04b44a5e3f9028c5a8eff6274c65ca8
chakrabortyayush1805-maker <chakrabortyayush1805@gmail.com>
2026-08-06 00:00:04 +0530

    ✅ Java Currency Formatter | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-05

--- hackerrank/medium/java-int-to-string/solution.java
+++ hackerrank/medium/java-int-to-string/solution.java
@@ -1,15 +1,24 @@
+import java.util.*;
+import java.text.*;
 
-static int B;
-    static int H;
-    static boolean flag = true;
-
-    static {
-        Scanner sc = new Scanner(System.in);
-        B = sc.nextInt();
-        H = sc.nextInt();
+public class Solution {
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double payment = scanner.nextDouble();
+        scanner.close();
         
-        if (B <= 0 || H <= 0) {
-            flag = false;
-            System.out.println("java.lang.Exception: Breadth and height must be positive");
-        }
+        // Write your code here.
+        Locale indiaLocale = new Locale("en", "IN");
+
+        String us = NumberFormat.getCurrencyInstance(Locale.US).format(payment);
+        String india = NumberFormat.getCurrencyInstance(indiaLocale).format(payment);
+        String china = NumberFormat.getCurrencyInstance(Locale.CHINA).format(payment);
+        String france = NumberFormat.getCurrencyInstance(Locale.FRANCE).format(payment);
+        
+        System.out.println("US: " + us);
+        System.out.println("India: " + india);
+        System.out.println("China: " + china);
+        System.out.println("France: " + france);
     }
+}
