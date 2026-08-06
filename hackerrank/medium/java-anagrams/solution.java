36ba065a1b8707e9b00037a8ccbe025b3caa5501
chakrabortyayush1805-maker <chakrabortyayush1805@gmail.com>
2026-08-06 20:25:22 +0530

    ✅ Java Anagrams | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-06

---
+++ hackerrank/medium/java-anagrams/solution.java
@@ -0,0 +1,27 @@
+
+
+    static boolean isAnagram(String a, String b) {
+        // Complete the function
+        a = a.toLowerCase();
+        b = b.toLowerCase();
+
+        if (a.length() != b.length()) {
+            return false;
+        }
+
+        int[] count = new int[26];
+
+        for (int i = 0; i < a.length(); i++) {
+            count[a.charAt(i) - 'a']++;
+            count[b.charAt(i) - 'a']--;
+        }
+
+        for (int c : count) {
+            if (c != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
