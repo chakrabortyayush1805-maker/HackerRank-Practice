3210ed4ce63084ae8b723dfdb76b3b9f9e030887
chakrabortyayush1805-maker <chakrabortyayush1805@gmail.com>
2026-08-07 21:46:56 +0530

    ✅ Java Regex 2 - Duplicate Words | Medium | Java | Runtime: N/A | Memory: N/A | 2026-08-07

--- hackerrank/medium/duplicate-word/solution.java
+++ hackerrank/medium/duplicate-word/solution.java
@@ -1,6 +1,31 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+public class DuplicateWords {
 
-class MyRegex {
-    String zeroTo255 = "(\\d{1,2}|(0|1)\\d{2}|2[0-4]\\d|25[0-5])";
-    public String pattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
+    public static void main(String[] args) {
+
+        String regex = "\\b(\\w+)(?:\\s+\\1)+\\b";
+        Pattern p = Pattern.compile(regex, Pattern.CASE_INSENSITIVE);
+
+        Scanner in = new Scanner(System.in);
+        int numSentences = Integer.parseInt(in.nextLine());
+        
+        while (numSentences-- > 0) {
+            String input = in.nextLine();
+            
+            Matcher m = p.matcher(input);
+            
+            // Check for subsequences of input that match the compiled pattern
+            while (m.find()) {
+                input = input.replaceAll(m.group(), m.group(1));
+            }
+            
+            // Prints the modified sentence.
+            System.out.println(input);
+        }
+        
+        in.close();
+    }
 }
